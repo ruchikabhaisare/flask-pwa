e5294ab17e1b2e636a1f72c5bca69c9a33710bf3
ruchikabhaisare <ruchika.21810325@viit.ac.in>
2021-10-09 22:55:21 +0530

    changes in service-worker

--- application.py
+++ application.py
@@ -6,6 +6,10 @@ application = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@application.route('/service-worker.js')
+def sw():
+    return application.send_static_file('service-worker.js')
+
 
 if __name__=='__main__':
-    application.run(host="0.0.0.0", port=8000, debug=True)
+    application.run(host="0.0.0.0", port=5000, debug=True)
